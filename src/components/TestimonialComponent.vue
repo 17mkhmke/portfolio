c047e236f9fb4a4b87103d428b4988c5639c8556
KingOfCoders <17mkhuselimkeyiya@gmail.com>
2023-05-16 10:36:54 +0200

    update

--- src/components/TestimonialComponent.vue
+++ src/components/TestimonialComponent.vue
@@ -30,8 +30,8 @@ export default{
           description: 'Mkhuseli has a great sense of humour, but is also serious and hardworking when it comes to his work. He has the ability to focus deeply on whatever tasks he finds before him. All in all, he is a great person to be around'
         },
         {
-          name: 'Athenkosi Freddie',
-          image: 'https://i.postimg.cc/Vvs5tgGr/image.jpg',
+          name: 'Gift Mahlangu',
+          image: 'https://i.postimg.cc/13yyjNp7/nkazana.jpg',
           description: 'He is a amazing person with a great sense of humor, a passion for creativity & is always hungry for greatness,never afraid to take risk & is willing to give a person a hand when needed. He is absolutely amazing.'
         },
         {
@@ -59,98 +59,118 @@ export default{
   }
 }
 </script>
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Quicksand:wght@500&display=swap');
-*{
-    margin: 0;
-    padding: 0;
+* {
+  margin: 0;
+  padding: 0;
 }
-.bbb{
-  background-color: #FDEEE3;
+
+.bbb {
+  background-color: #fdeee3;
   font-family: 'Mouse Memoirs', sans-serif;
-  /* min-height: 100vh; */
-  color:  #08233B;
-
-}
-.wrapper{
-    background-color: #FDEEE3;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: wrap;
-    font-family: 'Mouse Memoirs', sans-serif;
+  color: #08233B;
 }
 
-.box{
-    margin: 50px;
-    width: 300px;
-    height: 300px;
-    perspective: 1200px;
-}
-.single-box{
-    width: 300px;
-    height: 300px;
-    perspective: 1200px;
-}
-.box-content{
-    width: 100%;
-    height: 100%;
-    position: relative;
-    transform-style: preserve-3d;
-    transform: translateZ(-150px);
-    transition: all 1s;
-}
-.sides{
-    position: absolute;
-    width: 300px;
-    height: 300px;
-    text-align: center;
-}
-.side-1{
-    transform: rotateY(0deg) translateZ(150px);
-    z-index: 2;
-    background-size: cover;
-    background-position: center center;
-}
-.side-2{
-    color:  #FDEEE3;
-    background-color: #08233B;
-    transform: rotateY(90deg) translateZ(150px);
-}
-.box:hover .box-content{
-    transform: translateZ(-150px) rotateY(-90deg);
-}
-.content{
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    height: 100%;
-}
-.content h2{
+.wrapper {
+  background-color: #fdeee3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
   font-family: 'Mouse Memoirs', sans-serif;
-  color:  #FDEEE3;
-    font-size: 25px;
-}
-.content p{
-    font-size: 15px;
-    line-height: 25px;
-}
-.socials{
-    margin-top: 30px;
-}
-.socials i{
-    margin: 0 15px;
 }
 
-@media (max-width:991px){
-    .wrapper{
-        flex-direction: column;
-    }
+.box {
+  margin: 50px;
+  width: 300px;
+  height: 300px;
+  perspective: 1200px;
 }
-@media (max-width:991px){
-    .box{
-        width: 100%;
-    }
+
+.single-box {
+  width: 300px;
+  height: 300px;
+  perspective: 1200px;
+}
+
+.box-content {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: translateZ(-150px);
+  transition: all 1s;
+}
+
+.sides {
+  position: absolute;
+  width: 300px;
+  height: 300px;
+  text-align: center;
+}
+
+.side-1 {
+  transform: rotateY(0deg) translateZ(150px);
+  z-index: 2;
+  background-size: cover;
+  background-position: center center;
+}
+
+.side-2 {
+  color: #fdeee3;
+  background-color: #08233B;
+  transform: rotateY(90deg) translateZ(150px);
+}
+
+.box:hover .box-content {
+  transform: translateZ(-150px) rotateY(-90deg);
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+}
+
+.content h2 {
+  font-family: 'Mouse Memoirs', sans-serif;
+  color: #fdeee3;
+  font-size: 25px;
+}
+
+.content p {
+  font-size: 15px;
+  line-height: 25px;
+}
+
+.socials {
+  margin-top: 30px;
+}
+
+.socials i {
+  margin: 0 15px;
+}
+
+@media (max-width: 768px) {
+  .box {
+    width: calc(50% - 40px);
+    margin: 20px;
+  }
+}
+@media (max-width: 576px) {
+  .box {
+    width: calc(100% - 40px);
+    margin: 20px;
+  }
+}
+
+@media (max-width: 375px) {
+  .box {
+    width: calc(100% - 20px);
+    margin: 10px;
+  }
 }
 </style>
 
